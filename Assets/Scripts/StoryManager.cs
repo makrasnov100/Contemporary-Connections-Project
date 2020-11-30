ad9faf1d322d08cdaa7290072f1e62a23f574b23
Kostiantyn Makrasnov <makrasnov100@gmail.com>
2020-11-30 14:56:58 -0800

    Initial UI

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+
+    public static StoryManager instance;
+
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        //Setup the story line with all pathways
+        //Setup the initial step in the story
+    }
+
+}
